cb798ba06a9eb3fc096eb420a3637a0daef5c681
WinnieTheRaven <Winter@data.localdomain>
2026-08-17 17:44:30 -0600

    [advancing] getting something weird UnU

--- quine_aarch64.s
+++ quine_aarch64.s
@@ -5,8 +5,26 @@ _start:
 	mov x29, sp
 	str x20, [sp, #16]
 	mov x13, sp
-	ldr x9, =0x2020202020202020
-	ldr x10, =0x657A2E203A666675
+	ldr x9, =0x766f6d090a5d3631
+	ldr x10, =0x7073202c33317820
+	stp x9, x10, [sp, #-16]!
+	ldr x9, =0x3032782072747309
+	ldr x10, =0x23202c70735b202c
+	stp x9, x10, [sp, #-16]!
+	ldr x9, =0x20766f6d090a215d
+	ldr x10, =0x0a7073202c393278
+	stp x9, x10, [sp, #-16]!
+	ldr x9, =0x5b202c303378202c
+	ldr x10, =0x32332d23202c7073
+	stp x9, x10, [sp, #-16]!
+	ldr x9, =0x0a3a74726174735f
+	ldr x10, =0x3932782070747309
+	stp x9, x10, [sp, #-16]!
+	ldr x9, =0x6f69746365732e09
+	ldr x10, =0x0a747865742e206e
+	stp x9, x10, [sp, #-16]!
+	ldr x9, =0x6c61626f6c672e09
+	ldr x10, =0x0a74726174735f20
 	stp x9, x10, [sp, #-16]!
 	mov x14, sp
 	adrp x11, repetemp
@@ -17,18 +35,18 @@ _start:
 loadrepestuff:
 	ldrb w16, [x11], #1
 	strb w16, [x12], #1
-	add x15, x15, 1
+	add x15, x15, #1
 	cmp x15, #85
 	b.lt loadrepestuff
 resetadresses:
 	sub x12, x12, #85
 	mov x15, #0
 	mov x17, x14
-printsfirst:
+printfirst:
 	mov x8, #64
 	mov x0, #1
 	mov x1, x14
-	mov x2, #51
+	mov x2, #112
 	svc #0
 sandwich1:
 	ldrb w16, [x17], #1
@@ -61,8 +79,8 @@ resumeprinting:
 	mov x15, #0
 	mov x8, #64
 	mov x0, #1
-	mov x1, repestuff
-	mov x2, 85
+	ldr x1, =repestuff
+	mov x2, #85
 	svc #0
 	cmp x13, x17
 	b.lt sandwich1
@@ -81,67 +99,82 @@ exitstuff:
 	svc #0
 nibbletobyte:
 	cmp w18, #0x00
-	b.eq cerobyte
-cerobyte:
-	mov w19, #0x30
+	b.eq zerobyte
 	cmp w18, #0x01
 	b.eq onebyte
-onebyte:
-	mov w19, #0x31
 	cmp w18, #0x02
 	b.eq twobyte
-twobyte:
-	mov w19, #0x32
 	cmp w18, #0x03
 	b.eq threebyte
-threebyte:
-	mov w19, #0x33
 	cmp w18, #0x04
 	b.eq fourbyte
-fourbyte:
-	mov w19, #0x34
 	cmp w18, #0x05
 	b.eq fivebyte
-fivebyte:
-	mov w19, #0x35
 	cmp w18, #0x06
 	b.eq sixbyte
-sixbyte:
-	mov w19, #0x36
 	cmp w18, #0x07
 	b.eq sevenbyte
-sevenbyte:
-	mov w19, #0x37
 	cmp w18, #0x08
 	b.eq eigthbyte
-eigthbyte:
-	mov w19, #0x38
 	cmp w18, #0x09
 	b.eq ninebyte
-ninebyte:
-	mov w19, #0x39
 	cmp w18, #0x0a
 	b.eq abyte
-abyte:
-	mov w19, #0x61
 	cmp w18, #0x0b
 	b.eq bbyte
-bbyte:
-	mov w19, #0x62
 	cmp w18, #0x0c
 	b.eq cbyte
-cbyte:
-	mov w19, #0x63
 	cmp w18, #0x0d
 	b.eq dbyte
-dbyte:
-	mov w19, #0x64
 	cmp w18, #0x0e
 	b.eq ebyte
-ebyte:
-	mov w19, #0x65
 	cmp w18, #0x0f
 	b.eq fbyte
+zerobyte:
+	mov w19, #0x30
+	ret
+onebyte:
+	mov w19, #0x31
+	ret
+twobyte:
+	mov w19, #0x32
+	ret
+threebyte:
+	mov w19, #0x33
+	ret
+fourbyte:
+	mov w19, #0x34
+	ret
+fivebyte:
+	mov w19, #0x35
+	ret
+sixbyte:
+	mov w19, #0x36
+	ret
+sevenbyte:
+	mov w19, #0x37
+	ret
+eigthbyte:
+	mov w19, #0x38
+	ret
+ninebyte:
+	mov w19, #0x39
+	ret
+abyte:
+	mov w19, #0x61
+	ret
+bbyte:
+	mov w19, #0x62
+	ret
+cbyte:
+	mov w19, #0x63
+	ret
+dbyte:
+	mov w19, #0x64
+	ret
+ebyte:
+	mov w19, #0x65
+	ret
 fbyte:
 	mov w19, #0x66
 	ret
