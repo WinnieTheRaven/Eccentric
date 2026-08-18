8686bab590e0aa166e9005a561420d2d658ec70e
WinnieTheRaven <Winter@data.localdomain>
2026-08-17 21:05:31 -0600

    [Stucked] Dunno what's wrong with the indexes :c

--- quine_aarch64.s
+++ quine_aarch64.s
@@ -40,9 +40,10 @@ loadrepestuff:
 	b.lt loadrepestuff
 resetadresses:
 	sub x12, x12, #85
-	add x12, x12, #28
+	add x12, x12, #43
 	mov x15, #0
-	mov x17, x14
+	mov x17, x13
+	mov x20, x14
 printfirst:
 	mov x8, #64
 	mov x0, #1
@@ -50,41 +51,41 @@ printfirst:
 	mov x2, #112
 	svc #0
 sandwich1:
-	ldrb w16, [x17], #1
+	ldrb w16, [x17], #-1
 	and w18, w16, #0x0f
 	bl nibbletobyte
-	strb w19, [x12], #-1
+	strb w19, [x12], #1
 	and w18, w16, #0xf0
 	lsr w18 ,w18, #4
 	bl nibbletobyte
-	strb w19, [x12], #-1
+	strb w19, [x12], #1
 	add x15, x15, #1
 	cmp x15, #8
 	b.lt sandwich1
-	add x12, x12, #46
+	sub x12, x12, #46
 sandwich2:
-	ldrb w16, [x17], #1
+	ldrb w16, [x17], #-1
 	and w18, w16, #0x0f
 	bl nibbletobyte
-	strb w19, [x12], #-1
+	strb w19, [x12], 1
 	and w18, w16, #0xf0
 	lsr w18 ,w18, #4
 	bl nibbletobyte
-	strb w19, [x12], #-1
+	strb w19, [x12], #1
 	add x15, x15, #1
 	cmp x15, #16
 	b.lt sandwich2
 resumeprinting:
-	sub x12, x12, #42
-	add x12, x12, #28
+	sub x12, x12, #29
+	add x12, x12, #43
 	mov x15, #0
 	mov x8, #64
 	mov x0, #1
 	ldr x1, =repestuff
 	mov x2, #85
 	svc #0
-	cmp x17, x13
-	b.lt sandwich1
+	cmp x17, x20
+	b.gt sandwich1
 printlast:
 	mov x8, #64
 	mov x0, #1
